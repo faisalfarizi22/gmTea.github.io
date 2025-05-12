8b2b8f19983aebcba1203bd329e694c321447299
faisalfarizi22 <faisal.alfariziak@gmail.com>
2025-05-12 21:34:54 +0700

    remove connection status

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,11 +20,11 @@ function GMApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <ConnectionStatusProvider>
+      
         <ThirdwebProvider >
           <Component {...pageProps} />
         </ThirdwebProvider>
-      </ConnectionStatusProvider>
+      
     </>
   );
 }
